19f0b03a18ace6df20384d78acd5078eba1b80c3
chep1126 <772575249@qq.com>
2017-09-16 17:06:31 +0800

    v0.4.4 fix some bugs

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class Movie(db.Model):
 
 
 class Movie_info(db.Model):
-    __tablename__ = "Movie_info"
+    __tablename__ = "movie_info"
     id = db.Column(db.VARCHAR(20), primary_key=True)
     content = db.Column(db.TEXT)
     thunder_urls = db.Column(db.TEXT)
